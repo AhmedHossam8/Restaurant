dfa4fee6c0d2680ea630e58e23b7f65ce4a2fb39
gt <ahmedhossamemara8@gmail.com>
2022-04-04 01:54:54 +0200

    Enhancing

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-function transform_to_active(element){
-    element.style.textDecoration = 'underline'
+function transform_to_active(element) {
+    var els = document.getElementsByClassName("navbar-link");
+    
+    for(var i=0; i<els.length; i++){
+        els[i].style.color = "white"
+    }
+    element.style.color = 'rgb(193, 31, 31)'
 }
