374c9c2e784766df3a540e85fd8b538603e8f621
gt <ahmedhossamemara8@gmail.com>
2022-04-05 00:24:44 +0200

    added scroll to elements

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 function transform_to_active(element) {
     var els = document.getElementsByClassName("navbar-link");
-    
-    for(var i=0; i<els.length; i++){
+
+    for (var i = 0; i < els.length; i++) {
         els[i].style.color = "white"
     }
     element.style.color = 'rgb(193, 31, 31)'
+
+    var id = element.id;
+    console.log(id);
+    id = id + "_";
+    console.log(id);
+    /*const Myelement = document.getElementById(id);
+    Myelement.scrollIntoView();
+*/
+    var element = document.getElementById(id);
+    var headerOffset = 50;
+    var elementPosition = element.getBoundingClientRect().top;
+    var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+    window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth"
+    });
+
 }
